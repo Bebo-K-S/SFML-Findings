4850e8d21a4c85c0ecbc38ba2b080b03814cd002
Abdurlahman Khaled <107813045+Bebo-K-S@users.noreply.github.com>
2023-07-30 15:43:42 +0300

    Create Scene_Manager.cpp

---
+++ Scene_Manager.cpp
@@ -0,0 +1,27 @@
+/*Include source files*/
+
+int main()
+{
+    int* scene = 0;
+
+    while (*scene != -1)
+    {
+        switch (*scene)
+        {
+            case 0:
+                /*Go to respective scene function*/
+                break;
+            case 1:
+                /*Go to respective scene function*/
+                break;
+            /*
+              .
+              .
+              .
+              .
+            */
+        }
+    }
+
+    return 0;
+}
